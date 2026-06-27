1f55c80725315cfb65df79da616655cb3781d938
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-28 01:05:33 +0500

    chore: update dependencies and packages

--- src/taskpane/components/HeroList.tsx
+++ src/taskpane/components/HeroList.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { tokens, makeStyles } from "@fluentui/react-components";
-// import token, makes 
 export interface HeroListItem {
   icon: React.JSX.Element;
   primaryText: string;
