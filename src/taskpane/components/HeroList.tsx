093afdaa4053eaa1de510782491fac0f22da8f9e
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-06-28 01:05:01 +0500

    test: run integration tests for both qbo and xero

--- src/taskpane/components/HeroList.tsx
+++ src/taskpane/components/HeroList.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { tokens, makeStyles } from "@fluentui/react-components";
-// import token, makes style from fleunt
+// import token, makes 
 export interface HeroListItem {
   icon: React.JSX.Element;
   primaryText: string;
